86ab854567c0aaf7b43a25d4192d5c27421bf225
maximilianocaetano <maximilianocaetano@gmail.com>
2021-04-05 09:21:56 -0300

    leitura de todos os livros por categoria implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
 
 
@@ -40,6 +41,7 @@ import { CategoriaUpdateComponent } from './components/views/categoria/categoria
     CategoriaCreateComponent,
     CategoriaDeleteComponent,
     CategoriaUpdateComponent,
+    LivroReadAllComponent,
 
   ],
   imports: [
