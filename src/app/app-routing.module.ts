94a27bc244b26b3f214de98ac5a1bf1d59859e6b
maximilianocaetano <maximilianocaetano@gmail.com>
2021-04-05 07:44:03 -0300

    UPDATE para categoria implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { HomeComponent } from './components/views/home/home.component';
 
 const routes: Routes = [
   {
-  path: '',
-  component: HomeComponent
+    path: '',
+    component: HomeComponent
   },
   {
-  path: 'categorias',
-  component: CategoriaReadComponent
+    path: 'categorias',
+    component: CategoriaReadComponent
   },
   {
-  path: 'categorias/create',
-  component: CategoriaCreateComponent
+    path: 'categorias/create',
+    component: CategoriaCreateComponent
   },
   {
     path: 'categorias/delete/:id',
     component: CategoriaDeleteComponent
+  },
+  {
+    path: 'categorias/update/:id',
+    component: CategoriaUpdateComponent
   }
 ];
 
--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -30,6 +30,10 @@ export class CategoriaService {
     const url = `${this.baseUrl}/categorias/${id}`
     return this.http.delete<void>(url)
   }
+  update(categoria: Categoria):Observable<void> {
+    const url = `${this.baseUrl}/categorias/${categoria.id}`
+    return this.http.put<void>(url, categoria)
+  }
 
   mensagem(str: string): void{
     this._snack.open(`${str}`,'OK',{
