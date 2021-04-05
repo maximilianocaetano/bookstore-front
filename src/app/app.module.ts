e6f56bc3d7545bb6d210f5d612a1d68eb9dd1ddb
maximilianocaetano <maximilianocaetano@gmail.com>
2021-04-05 17:18:29 -0300

    UPDATE para livro implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { MatTableModule } from '@angular/material/table';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
+import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
+import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 
 
 
@@ -42,6 +44,8 @@ import { LivroReadAllComponent } from './components/views/livro/livro-read-all/l
     CategoriaDeleteComponent,
     CategoriaUpdateComponent,
     LivroReadAllComponent,
+    LivroCreateComponent,
+    LivroUpdateComponent,
 
   ],
   imports: [
@@ -60,6 +64,7 @@ import { LivroReadAllComponent } from './components/views/livro/livro-read-all/l
     MatInputModule,
     MatFormFieldModule,
     MatSnackBarModule,
+    ReactiveFormsModule,
 
   ],
   providers: [],
